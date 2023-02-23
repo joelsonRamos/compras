2db2dbfcb4c84d019a3175efd3b0f6e8fda16cd5
joelsonRamos <joelson.engcomp@gmail.com>
2023-02-23 16:11:19 +0000

    pagina de compras iniciada sem erro

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,32 +3,144 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
         <title>Compras Casa</title>
 
+      
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"  />
+        <link  href="{{asset('css/adminlte.css')}}" rel="stylesheet"/>
+          <!-- Google Font: Source Sans Pro -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
 
-
+    
     </head>
-    <body class="antialiased">
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-            @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                    @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
-                    @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+    <body class="hold-transition sidebar-mini">
+        <!-- Site wrapper -->
+      <div class="wrapper">
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+        <!-- Navbar -->
+        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+          <!-- Left navbar links -->
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+            </li>
 
-            <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
-                <h1>Teste</h1>
+          </ul>
+        </nav>
+        <!-- /.navbar -->
 
-
+        <!-- Main Sidebar Container -->
+        <aside class="main-sidebar sidebar-dark-primary elevation-4">
+          <!-- Sidebar -->
+          <div class="sidebar">
+            <!-- Sidebar user (optional) -->
+            <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+              
+              <div class="info">
+                <a href="#" class="d-block">Gestor de Compras Casa</a>
+              </div>
             </div>
+            <!-- Sidebar Menu -->
+            <nav class="mt-2">
+              <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                <!-- Add icons to the links using the .nav-icon class
+                with font-awesome or any other icon font library -->
+                <li class="nav-item">
+                  <a href="#" class="nav-link">
+                    <i class="nav-icon fa fa-cart-shopping"></i>
+                    <p>
+                      Compras
+                    </p>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a href="#" class="nav-link">
+                    <i class="nav-icon fa fa-cart-plus"></i>
+                    <p>
+                      Add novo Produto
+                    </p>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a href="#" class="nav-link">
+                    <i class="nav-icon fa fa-calendar"></i>
+                    <p>
+                      Compras Anteriores
+                    </p>
+                  </a>
+                </li>
+
+              </ul>
+            </nav>
+            <!-- /.sidebar-menu -->
+          </div>
+          <!-- /.sidebar -->
+        </aside>
+
+          <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+          <!-- Content Header (Page header) -->
+          <section class="content-header">
+            <div class="container-fluid">
+              <div class="row mb-2">
+                <div class="col-sm-6">
+                  <h1>Compras do Mês de: (Mês)</h1>
+                  
+                </div>
+
+              </div>
+            </div><!-- /.container-fluid -->
+          </section>
+
+          <!-- Main content -->
+          <section class="content">
+
+            <!-- Default box -->
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Title</h3>
+
+                <div class="card-tools">
+                  <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                    <i class="fas fa-minus"></i>
+                  </button>
+                  <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+                    <i class="fas fa-times"></i>
+                  </button>
+                </div>
+              </div>
+              <div class="card-body">
+                Start creating your amazing application!
+              </div>
+              <!-- /.card-body -->
+              
+              <!-- /.card-footer-->
+            </div>
+            <!-- /.card -->
+
+          </section>
+          <!-- /.content -->
         </div>
+        <!-- /.content-wrapper -->
+
+        <footer class="main-footer">
+          <div class="float-right d-none d-sm-block">
+            <b>Version</b> 2.0.1
+          </div>
+          <strong>Copyright &copy; 2023 </strong> All rights reserved Joelson Ramos Ferreira.
+        </footer>
+        
+      </div>
+      <!-- ./wrapper -->
+
+      <!-- jQuery -->
+      <script src="{{asset('js/jquery.js')}}"></script> 
+      <!-- Bootstrap 4 -->
+      <script src="{{asset('js/bootstrap.bundle.js')}}"></script> 
+      <!-- AdminLTE App -->
+      <script src="{{asset('js/adminlte.js')}}"></script>
+
     </body>
 </html>
